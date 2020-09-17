9d20e72d593e81076709a2df096121ad81affc96
fry <fry@6dreams.net>
2020-09-17 11:45:47 +0300

    #1: init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# OpenAPI Parser
+Allows you to load an openapi schema into usable objects, modify it and save the schema back.
+
+[![Build Status](https://travis-ci.org/6dreams/openapi-parser.svg?branch=master)](https://travis-ci.org/6dreams/openapi-parser)
---
+++ tests/TravisTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace SixDreams\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Temporary test for testing travis.
+ */
+class TravisTest extends TestCase
+{
+    /**
+     * Test.
+     */
+    public function testTravis(): void
+    {
+        self::assertTrue(true);
+    }
+}
